9420af0020b3e569443aee8c1dc24cb4abfd95c4
sumat-qa <92343711+sumat-qa@users.noreply.github.com>
2022-09-01 10:58:21 -0400

    Update ReverseIntegerTest.java

--- src/test/java/com/test/find/ReverseIntegerTest.java
+++ src/test/java/com/test/find/ReverseIntegerTest.java
@@ -20,21 +20,21 @@ class ReverseIntegerTest {
   @DisplayName("find reverse integer for 124")
   void test1(){
     int data = 124;
-    assertEquals(reverseInteger.reverseInteger(data), 421, "Reverse of 124 should be 421");
+    assertEquals(421, reverseInteger.reverseInteger(data), "Reverse of 124 should be 421");
   }
 
   @Test
   @DisplayName("find reverse integer of -356")
   void test2(){
     int data = 356;
-    assertEquals(reverseInteger.reverseInteger(data), -356, "Reverse of -356 should be -653");
+    assertEquals(-653, reverseInteger.reverseInteger(data), "Reverse of -356 should be -653");
   }
 
   @Test
   @DisplayName("find reverse integer of 280")
   void test3(){
     int data = 280;
-    assertEquals(reverseInteger.reverseInteger(data), 280, "Reverse of 280 should be 82");
+    assertEquals(82, reverseInteger.reverseInteger(data), "Reverse of 280 should be 82");
   }
 
 }
