af3e69238914b73dc2e58e7aa428214c7bc7b742
sumat-qa <92343711+sumat-qa@users.noreply.github.com>
2022-09-01 10:10:43 -0400

    Update and rename LongestSubString.java to ReverseInteger.java

---
+++ src/main/java/com/test/find/ReverseInteger.java
@@ -0,0 +1,17 @@
+package com.test.find;
+
+import java.util.List;
+
+/**
+ * Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0
+ */
+
+public class ReverseInteger {
+
+    public int reverseInteger(int input){
+        int reverseInteger=0;
+        //Put your code here and return expected results
+
+        return reverseInteger;
+    }
+}
